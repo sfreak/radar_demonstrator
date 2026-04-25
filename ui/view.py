99b5e3a9ad416b07e55069b7f311d0d272653ad8
Sebastian Brückner <sb@thebackend.de>
2026-04-25 09:29:00 +0200

    RDM is now labeled in range and speed

--- ui/view.py
+++ ui/view.py
@@ -20,26 +20,35 @@ class PlotRDM(PlotWidget):
             image=np.zeros((waveform.n_rg, waveform.n_dg)),
             levels=(40,100) # FIXME
         )
+
+        self.waveform = waveform
         
+        r_max = waveform.n_rg * waveform.d_range
+        v_max = waveform.n_dg * waveform.d_speed / 2
         tr = QtGui.QTransform()
-        tr.translate(0, -waveform.n_dg//2) # move DG32 to 0
+        tr.translate(0, -v_max) # move DG32 to 0
+        tr.scale(
+            waveform.d_range, # RG to m
+            waveform.d_speed # DG to m/s
+        )
         self.img.setTransform(tr) # assign transform
         self.img.setColorMap(pg.colormap.get('viridis'))
 
-        self.setRange(yRange=(-waveform.n_dg//2, waveform.n_dg//2-1), xRange=(0, waveform.n_rg), padding=0)
+        self.setRange(yRange=(-v_max, v_max), xRange=(0, r_max), padding=0)
         self.addItem(self.img)
 
-        self.setLabel('bottom', 'Range Gate')
-        self.setLabel('left', 'Doppler Gate')
+        self.setLabel('bottom', 'Range / m')
+        self.setLabel('left', 'Radial Speed / m/s')
 
         # target list overlay on RDM
         self.sc = pg.ScatterPlotItem(x=[], y=[])
         self.addItem(self.sc)
 
     def newTargets(self, targets:list[dict]):
-        rg = [t.get('rg')+0.5 for t in targets]
-        dg = [t.get('dg')+0.5 for t in targets]
-        self.sc.setData(x=rg, y=dg)
+        # FIXME should this conversion be done Controller instead?
+        r = [(t.get('rg')+0.5)*self.waveform.d_range for t in targets]
+        v = [(t.get('dg')+0.5)*self.waveform.d_speed for t in targets]
+        self.sc.setData(x=r, y=v)
 
     def newRDM(self, rdm:np.ndarray):
         imdata = np.fft.fftshift(rdm, axes=1)
@@ -50,17 +59,20 @@ class PlotRangeProfile(PlotWidget):
     def __init__(self, waveform:Waveform, parent=None, background='default', plotItem=None, **kargs):
         super().__init__(parent, background, plotItem, **kargs)
 
+        self.x = np.arange(waveform.n_rg) * waveform.d_range
+
         # Range profile plot
         self.trace0 = self.plot(
             x=[], y=[],
             pen=pg.mkPen('black', width=3),
             symbol='o')
         self.setYRange(30, 110) # FIXME
-        self.setLabel('left', 'Received Power')
-        self.setLabel('bottom', 'Range Gate')
+        self.setXRange(0, waveform.n_rg*waveform.d_range, padding=0)
+        self.setLabel('left', 'Received Power / dB')
+        self.setLabel('bottom', 'Range / m')
     
     def newRangeProfile(self, Pr:np.ndarray):
-        self.trace0.setData(Pr)
+        self.trace0.setData(self.x, Pr)
 
 
 class PlotVelocity(PlotWidget):
@@ -77,11 +89,11 @@ class PlotVelocity(PlotWidget):
         self.setYRange(0, max_v)
         self.setXRange(-20, 0) # FIXME
         self.setLabel('bottom', 'Time / s')
-        self.setLabel('left', '|Speed| / m/s')
+        self.setLabel('left', '|Radial Speed| / m/s')
         # highscore
-        self.speed_template = '<span style="color: red; font-size: 16pt;">Highest speed: {: 4.1f} m/s</span>'
+        self.speed_template = '<span style="color: red; font-size: 16pt;">Highest radial speed: {: 4.1f} m/s</span>'
         self.speed_text = pg.TextItem(html=self.speed_template.format(0))
-        self.speed_text.setPos(-14,4) # position in data coordinates (i.e., time and speed), FIXME
+        self.speed_text.setPos(-18,4) # position in data coordinates (i.e., time and speed), FIXME
         self.addItem(self.speed_text)
 
     def newSpeeds(self, t:list, v:list):
